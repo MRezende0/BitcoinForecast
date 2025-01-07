3966edcd33a2a2d009d77869b9cdaea72e810235
Matheus Rezende <matheus_rezende0@hotmail.com>
2025-01-07 15:12:42 -0300

    update

--- grabber.py
+++ grabber.py
@@ -1,34 +1,4 @@
-#!/usr/bin/python3
-
-##
 ## run the code for about 2/3 days
-##
-
-# import requests
-# import time
-
-# f_name = input("dataset name:")
-# f = open(f_name,"a")
-# keys = ["price_usd","24h_volume_usd","market_cap_usd","available_supply","total_supply","percent_change_1h","percent_change_24h","percent_change_7d"]
-# vals = [0]*len(keys)
-
-# while True:
-#   data = requests.get("https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT").json()
-#   bstamp = requests.get("https://www.bitstamp.net/api/v2/ticker/btcusd/").json() 
-#   bkc = requests.get("https://blockchain.info/ticker").json()
-  
-#   for d in data.keys():
-#      if d in keys:
-#        indx = keys.index(d)
-#        vals[indx] = data[d]
-#   for val in vals:
-#        f.write(val+",")
-      
-#   f.write("{},{},".format(bstamp["volume"],bstamp["vwap"]))
-#   f.write("{},{},{}".format(bkc["USD"]["sell"],bkc["USD"]["buy"],bkc["USD"]["15m"]))
-#   f.write("\n")
-#   f.flush()
-#   time.sleep(9*60)
 
 import requests
 import time
